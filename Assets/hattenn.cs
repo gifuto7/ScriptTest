a51e5a88a5444d09e170b88f1e232dc7c8d8ab5d
森長湧登 <pinntiru3@yahoo.co.jp>
2016-11-06 21:58:25 +0900

    [add]ScriptのTest
    初めてのスクリプトのコーディング　しっかりと動作するかテストしました

---
+++ Assets/hattenn.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hattenn : MonoBehaviour
+{
+
+    //変数初期化
+    private int hp = 100;   //体力
+    private int power = 25; //攻撃力
+    int mp = 53;    //マジックポイント
+    string mozi = "魔法攻撃をした。残りMPは"; 
+    string mozi2 = "。";
+
+    //魔法攻撃用関数
+    public void Magic(int cons)
+    {
+        //MPが足りなくなるまで魔法を使い続ける。
+        for (int i = mp; i >= cons; i -= cons)
+        {
+            //いくらMPを消費したか表示
+            Debug.Log(mozi + i.ToString() + mozi2);
+        }
+        //MPがなくなると表示
+        Debug.Log("MPが足りないため魔法が使えない");
+    }
+    //攻撃用関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    //防御用関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた");
+        //残りhpをへらす
+        this.hp -= damage;
+    }
+    void Start()
+    {
+
+        //魔法用の関数を呼び出す
+        Magic(5);
+        // 攻撃用の関数を呼び出す
+        Attack();
+        // 防御用の関数を呼び出す
+        Defence(3);
+    }
+
+    void Update()
+    {
+
+    }
+
+
+
+
+
+
+}
+
+
+ 
+
+
+
+
---
+++ Assets/kadai.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kadai : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        //配列を初期化
+        int[] array = { 3, 5, 7, 9, 1 };
+
+        Debug.Log("配列の要素を表示");
+        //配列を要素数の分だけ処理を繰り返す
+        for (int i = 0; i < array.Length; i++)
+        {
+            //配列の要素を表示
+            Debug.Log(array[i]);
+        }
+
+        Debug.Log("配列の要素を反対に表示");
+        //配列を要素数の分だけ逆に表示
+        for (int i = array.Length; i >= 0; i--)
+        {
+            //iと配列の要素数が等しいくない場合に実行
+            if (i != array.Length)
+            {
+                Debug.Log(array[i]);
+            }
+        }
+    }
+}
+        // Update is called once per frame
