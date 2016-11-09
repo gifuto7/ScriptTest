6fcc005cd00429227ccfdd999af732e673f4bdb4
森長湧登 <pinntiru3@yahoo.co.jp>
2016-11-09 21:52:21 +0900

    [fix]MPを消費するが魔法が使えないバグの修正
    MPの処理をforではなくwhileに変更しました。

---
+++ Assets/hattenn.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hattenn : MonoBehaviour
+{
+
+    public int hp = 100;
+    public int power = 25;
+    int mp = 53;
+    string mozi = "魔法攻撃をした。残りMPは";
+    string mozi2 = "。";
+    string mozi3 = "MPが足りまいため魔法が使えない";
+
+    //魔法用関数で真偽値格納
+    public bool Magic(int cons)
+    {
+        //魔法を使うためのMPが足りるかを判定
+        if (mp >= cons)
+        {
+            //足りたためwhileにtrueを返す。
+
+            return true;
+        }
+        else
+        {
+           //足りなかったためwhileにfalseを返す。
+            return false;
+        }
+    }
+
+    //攻撃用関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    //防御用関数
+    public void Defence(int damage)
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+        //残りhpを減らす
+        this.hp -= damage;
+    }
+
+    void Start()
+    {
+        int mpcons = 5;
+
+        //falseが返されるまでステートメンのブロックを毎度実行
+        while (Magic(mpcons))
+        {
+            //MPから消費分の数値を引く
+            mp -= mpcons;
+
+            Debug.Log(mozi + mp.ToString() + mozi2);
+        }
+        //whileから抜けると表示
+        Debug.Log("MPが足りないため魔法が使えない。");
+    }
+}
+
---
+++ Assets/kadai.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class kadai : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //配列の初期化
+        int[] array = { 5, 9, 7, 2, 3 };
+
+        Debug.Log("配列を順番に表示");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        Debug.Log("配列を逆から順番に表示");
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
